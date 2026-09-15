708baaeacfae634a0dd27a010012f0b388f6fdf8
Kamil Frem <137979579+KamilFrem@users.noreply.github.com>
2026-09-14 17:46:58 -0700

    Fix infinite loop in count_occurence program

--- 2-debug/debug.s
+++ 2-debug/debug.s
@@ -2,7 +2,9 @@
 #  Max Score: 9 points
 #
 # Students: 
-#
+# Kamil, Ian and Macabe 
+# We all worked on this together, 33% for each
+
 # 'count_occurence.a' - count the occurrences of a specific character in string 
 # "str". Indexed addressing is used to access the array elements.
 #  MAX Score: 15 points
