cfe956b16d41d90faf6e549b05b404745ebc9c91
Kamil Frem <137979579+KamilFrem@users.noreply.github.com>
2026-09-14 17:47:54 -0700

    Update comments and student credits in recursive_mystery1.s

--- 4-recursion/recursive_mystery1.s
+++ 4-recursion/recursive_mystery1.s
@@ -1,5 +1,8 @@
 # Exercise 3
 # Max Score: 12 points
+# Students: 
+# Kamil, Ian and Macabe 
+# We all worked on this together, 33% for each
 .data	
 list1:		.word		3, 9, 1, 2, 6, 3, -4, -7, -8, 4, -2,  8, 7, 6
 .text 		# list1 is an array of integers storing the given sequence of values	 
