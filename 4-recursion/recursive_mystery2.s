19ddd272d638f3d2738317c1646effddbb914dba
Kamil Frem <137979579+KamilFrem@users.noreply.github.com>
2026-09-14 17:48:05 -0700

    Add student names and contributions to recursive_mystery2.s

--- 4-recursion/recursive_mystery2.s
+++ 4-recursion/recursive_mystery2.s
@@ -1,5 +1,8 @@
 # Exercise 3
 # Max Score: 12 points
+# Students: 
+# Kamil, Ian and Macabe 
+# We all worked on this together, 33% for each
 .data	
 list1:		.word		3, 9, -1, 0, 6, 5, -4, -7, -8,  
 list2:		.word		9, 5, 0, 3, -4, 5, 6, -7, 8, 9, 
